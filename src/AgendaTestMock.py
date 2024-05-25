ecd817caa7a7f4b44d6750d35b8578516017741b
Nicholas Siécola <n.siecola@gec.inatel.br>
2024-05-25 16:12:01 -0300

    add test_createShow

--- src/AgendaTestMock.py
+++ src/AgendaTestMock.py
@@ -8,6 +8,12 @@ class AgendaTestMock(unittest.TestCase):
     def test_init(self):
         agenda = Agenda()
         self.assertEqual([], agenda.readShows())
+
+    def test_createShow(self):
+        agenda = Agenda()
+        show_mock = MagicMock(spec=Show)
+        agenda.createShow(show_mock)
+        self.assertEqual([show_mock], agenda.readShows())
         
 if __name__ == '__main__':
     unittest.main()
